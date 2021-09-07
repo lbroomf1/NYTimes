97b9c0ca8b24170b97511408676137795a6d7351
lbroomf1 <lawrence.broomfield@gmail.com>
2021-09-07 19:43:16 +0530

    Added TopStories veiwModle logic to fetch topstories from Repository

---
+++ NYTimes/Modules/TopStories/TopStoryDetails.swift
@@ -0,0 +1,14 @@
+//
+//  NewsDetails.swift
+//  NYTimes
+//
+//  Created by MAC on 07/09/21.
+//
+
+import Foundation
+
+struct TopStoryDetails  {
+    let title: String
+    let byline: String
+    let updatedDate: String
+}
